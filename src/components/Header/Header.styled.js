7bd5c62bdca5a64912768d8ac98299ce698e2d3d
Sveta <svetikred77@gmail.com>
2024-01-19 19:35:40 +0200

    fix public

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-// import Select from 'react-select';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -24,31 +23,3 @@ export const Heading = styled.div`
     margin-right: 20px;
   }
 `;
-
-// export const CustomSelect = styled(Select).attrs({
-//   styles: {
-//     control: baseStyles => ({
-//       ...baseStyles,
-//       width: '194px',
-//       height: '37px',
-//       borderRadius: '10px',
-//       color: 'var(--background-theme)',
-//       background: 'var(--bg-select)',
-//       border: 'none',
-//     }),
-//     option: provided => ({
-//       ...provided,
-//       cursor: 'pointer',
-//       color: 'var(--color-text)',
-//       backgroundColor: 'var(--background)',
-//     }),
-//   },
-// })`
-//   & * {
-//     color: var(--color-subText) !important;
-//   }
-
-//   & > div[id] {
-//     background-color: var(--background);
-//   }
-// `;
